8c1a529c3d44b9d53bebd3aba7ff7cd6b1a772ec
alulab14 <alulab14@inf.pucp.edu.pe>
2019-10-31 10:19:22 -0500

    Acabando con el server

--- MarketSoftServices/src/java/pe/edu/pucp/marketsoft/services/Servicio.java
+++ MarketSoftServices/src/java/pe/edu/pucp/marketsoft/services/Servicio.java
@@ -5,9 +5,12 @@
  */
 package pe.edu.pucp.marketsoft.services;
 
+import java.util.ArrayList;
 import javax.jws.WebService;
 import javax.jws.WebMethod;
 import javax.jws.WebParam;
+import pe.edu.pucp.marketsoft.config.DBController;
+import pe.edu.pucp.marketsoft.model.Promocion;
 
 /**
  *
@@ -22,5 +25,25 @@ public class Servicio {
     @WebMethod(operationName = "hello")
     public String hello(@WebParam(name = "name") String txt) {
         return "Hello " + txt + " !";
+    }
+    
+    @WebMethod(operationName = "insertarPromociones")
+    public int insertarPromociones(@WebParam(name = "Promocion") Promocion promocion){
+        return DBController.insertarPromocion(promocion);
+    }
+    
+    @WebMethod(operationName = "actualizarPromociones")
+    public int actualizarPromociones(@WebParam(name = "Promocion") Promocion promocion){
+        return DBController.actualizarPromocion(promocion);
+    }
+    
+    @WebMethod(operationName = "eliminarPromociones")
+    public int eliminarPromociones(@WebParam(name = "id_promocion") int id_promocion){
+        return DBController.eliminarPromocion(id_promocion);
+    }
+    
+    @WebMethod(operationName = "listarPromociones")
+    public ArrayList<Promocion> listarPromociones(@WebParam(name = "nombre") String nombre){
+        return DBController.listarPromociones(nombre);
     }
 }
